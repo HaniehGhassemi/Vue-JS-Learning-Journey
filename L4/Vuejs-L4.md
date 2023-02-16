ba9bdd9e6fc3d9cddb65b43975b46214a92e32b7
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-16 10:44:58 +0330

    Conditional-Rendering

---
+++ L4/Vuejs-L4.md
@@ -0,0 +1,7 @@
+<h1>Vue.js Lesson 3</h1>
+
+<h2>:writing_hand: What I learned?</h2>
+<ul>
+<li>Conditional Rendering</li>
+<li>Toggle between hiding and showing an Element</li>
+</ul>
---
+++ L4/main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+      url: "https://github.com/HaniehGhassemi",
+      inStock: 8,
+      toggling: true,
+      onSale: trfue
+    };
+  },
+});
